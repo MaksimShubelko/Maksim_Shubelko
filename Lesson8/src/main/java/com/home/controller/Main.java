6497cc30eb299948315afd170caee2169b245796
MaksimShubelko <86020437+MaksimShubelko@users.noreply.github.com>
2021-07-14 16:43:06 +0300

    Lesson8

--- Lesson8/src/main/java/com/home/controller/Main.java
+++ Lesson8/src/main/java/com/home/controller/Main.java
@@ -97,7 +97,6 @@ public class Main {
                 default:
                     result = "Неправильные данные";
                     printer.printResult(result);
-
             }
         }
     }
