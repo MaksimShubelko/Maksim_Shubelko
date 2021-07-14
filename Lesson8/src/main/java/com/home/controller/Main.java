1d25566ca20e48a6daf7523e977e27e449f9c25d
MaksimShubelko <86020437+MaksimShubelko@users.noreply.github.com>
2021-07-14 22:01:36 +0300

    Lesson8

--- Lesson8/src/main/java/com/home/controller/Main.java
+++ Lesson8/src/main/java/com/home/controller/Main.java
@@ -9,10 +9,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String carModel;
-        int yearRelease;
-        double totalDistance;
-
         InputValidation inputValidation = new InputValidation();
         Printer printer = new Printer();
         int chose;
@@ -40,64 +36,80 @@ public class Main {
                     double fuelConsumption = inputValidation.checkDouble();
 
                     Car car = new Car(engine, gasTank, distance, fuelConsumption);
-
-                    while (true) {
-
-                        System.out.println("Выберите действие:\n1 -- завести\n2 -- заглушить\n" +
-                                "3 -- ехать\n4 -- посмотреть пробег\n5 -- посмотреть кол-во топлива\n" +
-                                "6 -- заправить\n7 -- добавить больше данных о машине\n" +
-                                "8 -- посмотреть данные о транспорте\n9 -- выход");
-                        chose = inputValidation.checkInt();
-
-                        switch (chose) {
-                            case 1:
-                                result = car.engineOn();
-                                break;
-                            case 2:
-                                result = car.engineOff();
-                                break;
-                            case 3:
-                                result = car.ride();
-                                break;
-                            case 4:
-                                result = car.checkDistance();
-                                break;
-                            case 5:
-                                result = car.calculateFuelLevel();
-                                break;
-                            case 6:
-                                result = car.refuelCar();
-                                break;
-                            case 7:
-                                System.out.println("Введите марку машины:");
-                                carModel = scanner.nextLine();
-                                car.setCarModel(carModel);
-
-                                System.out.println("Введите год выпуска:");
-                                yearRelease = inputValidation.checkInt();
-                                car.setYearRelease(yearRelease);
-
-                                System.out.println("Введите пробег:");
-                                totalDistance = inputValidation.checkDouble();
-                                car.setTotalDistance(totalDistance);
-                                result = "Данные добавлены";
-                                break;
-                            case 8:
-                                result = car.toString();
-                                break;
-                            case 9:
-                                return;
-                            default:
-                                result = "Неправильные данные";
-                        }
-                        printer.printResult(result);
-                    }
+                    carMenu(car);
+                    break;
                 case 2:
                     return;
                 default:
                     result = "Неправильные данные";
                     printer.printResult(result);
             }
+        }
+
+
+    }
+
+    public static void carMenu(Car car) {
+        InputValidation inputValidation = new InputValidation();
+        Printer printer = new Printer();
+        Scanner scanner = new Scanner(System.in);
+
+        String result;
+        int chose;
+        String carModel;
+        int yearRelease;
+        double totalDistance;
+
+        while (true) {
+
+            System.out.println("Выберите действие:\n1 -- завести\n2 -- заглушить\n" +
+                    "3 -- ехать\n4 -- посмотреть пробег\n5 -- посмотреть кол-во топлива\n" +
+                    "6 -- заправить\n7 -- добавить больше данных о машине\n" +
+                    "8 -- посмотреть данные о транспорте\n9 -- выход");
+            chose = inputValidation.checkInt();
+
+            switch (chose) {
+                case 1:
+                    result = car.engineOn();
+                    break;
+                case 2:
+                    result = car.engineOff();
+                    break;
+                case 3:
+                    result = car.ride();
+                    break;
+                case 4:
+                    result = car.checkDistance();
+                    break;
+                case 5:
+                    result = car.calculateFuelLevel();
+                    break;
+                case 6:
+                    result = car.refuelCar();
+                    break;
+                case 7:
+                    System.out.println("Введите марку машины:");
+                    carModel = scanner.nextLine();
+                    car.setCarModel(carModel);
+
+                    System.out.println("Введите год выпуска:");
+                    yearRelease = inputValidation.checkInt();
+                    car.setYearRelease(yearRelease);
+
+                    System.out.println("Введите пробег:");
+                    totalDistance = inputValidation.checkDouble();
+                    car.setTotalDistance(totalDistance);
+                    result = "Данные добавлены";
+                    break;
+                case 8:
+                    result = car.toString();
+                    break;
+                case 9:
+                    return;
+                default:
+                    result = "Неправильные данные";
+            }
+            printer.printResult(result);
         }
     }
 }
