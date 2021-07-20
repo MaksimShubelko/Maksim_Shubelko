ef31aaea3d495603aa930f83551d1dbb0254512a
MaksimShubelko <86020437+MaksimShubelko@users.noreply.github.com>
2021-07-20 20:52:10 +0300

    Lesson10

--- Lesson10/src/main/java/tasks/first/com/home/model/heads/IHead.java
+++ Lesson10/src/main/java/tasks/first/com/home/model/heads/IHead.java
@@ -1,7 +1,12 @@
 package tasks.first.com.home.model.heads;
 
 import tasks.first.com.home.utils.IRobotParts;
+import tasks.first.com.home.utils.Types;
 
 public interface IHead extends IRobotParts {
     String speak();
+
+    default int getType() {
+        return Types.IHead.ordinal();
+    }
 }
