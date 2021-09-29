0f625cfe2d529453140a89a436c3043326d8b943
MaksimShubelko <86020437+MaksimShubelko@users.noreply.github.com>
2021-09-29 19:44:09 +0300

    Cloud

---
+++ Util/src/main/java/org/example/util/NameUtil.java
@@ -0,0 +1,8 @@
+package org.example.util;
+
+public class NameUtil {
+    public static String getFullName(String firstName, String lastName) {
+
+        return firstName + " " + lastName;
+    }
+}
